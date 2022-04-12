fe275ea5a5236cf06e42ddf0ee79d219638aa5ed
Daniel <danielw96@hotmail.com>
2022-04-12 18:13:07 -0400

    Create PokemonSearchData.java
    for adding search functionality  to app as api doesn't have it built in

---
+++ app/src/main/java/com/example/pokemonapp/PokemonSearchData.java
@@ -0,0 +1,11 @@
+package com.example.pokemonapp;
+
+public class PokemonSearchData {
+    int id;
+    String name;
+
+    public PokemonSearchData(int id, String name){
+        this.id = id;
+        this.name = name;
+    }
+}
