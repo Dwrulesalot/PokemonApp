943f534605cecc60fd79a4da88fcd4ba5fa991f2
Daniel <danielw96@hotmail.com>
2022-04-14 12:08:11 -0400

    added recycler to saved activity

--- app/src/main/java/com/example/pokemonapp/SavedPokemonActivity.java
+++ app/src/main/java/com/example/pokemonapp/SavedPokemonActivity.java
@@ -1,14 +1,36 @@
 package com.example.pokemonapp;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class SavedPokemonActivity extends AppCompatActivity {
+
+    RecyclerView savedRecyclerView;
+    PokemonRecyclerAdapter pokemonRecyclerAdapter;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_saved_pokemon);
+
+        savedRecyclerView = (RecyclerView) findViewById(R.id.savedRecyclerView);
+
+        //todo change below to display based on what pokemon are in the database
+        /*
+        pokemonRecyclerAdapter = new PokemonRecyclerAdapter(exampleListPokemonData, this);
+        pokemonListRecyclerView.setAdapter(pokemonRecyclerAdapter);
+        pokemonListRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        */
+    }
 
     //Menu Creation
     public boolean onCreateOptionsMenu(Menu menu){
